c461e51236d751426610094014ad6721e2070141
Ritter, Guilherme Alan <ritterg2@universalleaf.com>
2026-03-05 14:46:09 -0300

    done

--- src/main/java/io/github/guiritter/image_similar_finder/ImageSimilarFinder.java
+++ src/main/java/io/github/guiritter/image_similar_finder/ImageSimilarFinder.java
@@ -1,19 +1,16 @@
 package io.github.guiritter.image_similar_finder;
 
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+import static java.lang.System.exit;
+import static java.lang.System.out;
 import static javax.swing.JFileChooser.APPROVE_OPTION;
 import static javax.swing.JFileChooser.DIRECTORIES_ONLY;
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.IntConsumer;
-import java.util.function.Predicate;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
@@ -23,74 +20,59 @@ import javax.swing.JFrame;
 
 import org.joml.Vector3f;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class ImageSimilarFinder {
 
 	private static File candidateFolder;
-	private static File targetFolder;
-
 	private static Map<String, Vector3f> candidateAverageByImageFilePath = new HashMap<>();
+	
+	private static File targetFolder;
 	private static Map<String, Vector3f> targetAverageByImageFilePath = new HashMap<>();
+	
+	public static final Vector3f vectorMaximum = new Vector3f(255, 255, 255);
+	public static final Vector3f vectorMinimum = new Vector3f(0, 0, 0);
+
+	public static final float vectorDistanceMaximum = vectorMinimum.distance(vectorMaximum);
 
 	static {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JDialog.setDefaultLookAndFeelDecorated(true);
 	}
 
-	private static Predicate<ManagerCell> byLowerField(String lowerField) {
-		return managerCell -> managerCell.lowerField.compareToIgnoreCase(lowerField) == 0;
+	private static final void findSimilar(String targetImageFilePath, Vector3f targetAverage) {
+		var vectorDistanceSmallest = vectorDistanceMaximum;
+		float vectorDistance;
+		var candidateImageFilePathSmallest = "";
+
+		for (var candidate : candidateAverageByImageFilePath.entrySet()) {
+			vectorDistance = targetAverage.distance(candidate.getValue());
+
+			if (vectorDistance < vectorDistanceSmallest) {
+				vectorDistanceSmallest = vectorDistance;
+				candidateImageFilePathSmallest = candidate.getKey();
+			}
+		}
+
+		out.format("for\n%s\nmost similar is\n%s\n\n", targetImageFilePath, candidateImageFilePathSmallest);
 	}
 
-	private static final Stream<ManagerCell> flatten(ManagerColumn managerColumn) {
-		return managerColumn.cellList.stream();
+	private static final void storeCandidateAverage(File imageFile) {
+		storeAverage(imageFile, candidateAverageByImageFilePath);
 	}
 
-	private static final IntConsumer treatCell(WikiColumn wikiColumn, ManagerTable managerTable,
-			ManagerColumn bridgeColumn) {
-		return cellIndex -> {
-			var wikiCell = wikiColumn.cellList.get(cellIndex);
-			var lowerField = wikiCell.blk;
-
-			var managerCell = managerTable.columnList.stream().flatMap(ImageSimilarFinder::flatten)
-					.filter(byLowerField(lowerField)).findAny();
-
-			var upperField = "";
-
-			if (managerCell.isPresent()) {
-				upperField = managerCell.get().upperField;
-				lowerField = managerCell.get().lowerField;
-			}
-
-			while (bridgeColumn.cellList.size() <= cellIndex) {
-				bridgeColumn.cellList.add(new ManagerCell());
-			}
-
-			var bridgeCell = bridgeColumn.cellList.get(cellIndex);
-			bridgeCell.upperField = upperField;
-			bridgeCell.lowerField = lowerField;
-		};
+	private static final void storeTargetAverage(File imageFile) {
+		storeAverage(imageFile, targetAverageByImageFilePath);
 	}
-
-	private static final IntConsumer treatColumn(WikiTable wikiTable, ManagerTable managerTable,
-			ManagerTable bridgeTable) {
-		return columnIndex -> {
-
-			while (bridgeTable.columnList.size() <= columnIndex) {
-				bridgeTable.columnList.add(new ManagerColumn());
-			}
-
-			var wikiColumn = wikiTable.columnList.get(columnIndex);
-			var bridgeColumn = bridgeTable.columnList.get(columnIndex);
-
-			IntStream.range(0, wikiColumn.cellList.size()).forEach(treatCell(wikiColumn, managerTable, bridgeColumn));
-		};
-	};
 
 	private static final void storeAverage(File imageFile, Map<String, Vector3f> averageByImageFilePath) {
-		var bufferedImage = ImageIO.read(imageFile);
+		BufferedImage bufferedImage;
 
-		var colorModel = bufferedImage.getColorModel();
+		try {
+			bufferedImage = ImageIO.read(imageFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+			exit(0);
+			return;
+		}
 
 		var width = bufferedImage.getWidth();
 		var height = bufferedImage.getHeight();
@@ -101,7 +83,7 @@ public class ImageSimilarFinder {
 
 		Color color;
 
-		long r, g, b;
+		long r = 0, g = 0, b = 0;
 
 		for (y = 0; y < height; y++) {
 			for (x = 0; x < width; x++) {
@@ -120,32 +102,6 @@ public class ImageSimilarFinder {
 		);
 
 		averageByImageFilePath.put(imageFile.getAbsolutePath(), vector);
-
-		// var jsonMapper = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false).enable(INDENT_OUTPUT);
-		// WikiTable wikiTable;
-		// try {
-		// 	wikiTable = jsonMapper.readValue(wikiFile, WikiTable.class);
-		// } catch (IOException e) {
-		// 	e.printStackTrace();
-		// 	return;
-		// }
-		// var managerFile = candidateFolder.toPath().resolve(wikiFile.getName()).toFile();
-		// ManagerTable managerTable;
-		// try {
-		// 	managerTable = jsonMapper.readValue(managerFile, ManagerTable.class);
-		// } catch (IOException e) {
-		// 	e.printStackTrace();
-		// 	return;
-		// }
-		// var bridgeTable = new ManagerTable();
-		// IntStream.range(0, wikiTable.columnList.size()).forEach(treatColumn(wikiTable, managerTable, bridgeTable));
-
-		// var bridgeFile = bridgeFolder.toPath().resolve(wikiFile.getName()).toFile();
-		// try {
-		// 	jsonMapper.writeValue(bridgeFile, bridgeTable);
-		// } catch (IOException e) {
-		// 	e.printStackTrace();
-		// }
 	}
 
 	public static void main(String args[]) throws IOException {
@@ -167,13 +123,14 @@ public class ImageSimilarFinder {
 			if (chooser.showOpenDialog(null) != APPROVE_OPTION) {
 				return;
 			}
+			candidateFolder = chooser.getSelectedFile();
+			if (candidateFolder == null) {
+				return;
+			}
 		}
 
-		Consumer<File> storeTargetAverage = (imageFile) -> storeAverage(imageFile, targetAverageByImageFilePath);
-		Consumer<File> storeCandidateAverage = (imageFile) -> storeAverage(imageFile, candidateAverageByImageFilePath);
-
-		Stream.of(targetFolder.listFiles()).forEach(storeTargetAverage);
-		Stream.of(candidateFolder.listFiles()).forEach(storeCandidateAverage);
-		// TODO check one against the other
+		Stream.of(targetFolder.listFiles()).forEach(ImageSimilarFinder::storeTargetAverage);
+		Stream.of(candidateFolder.listFiles()).forEach(ImageSimilarFinder::storeCandidateAverage);
+		targetAverageByImageFilePath.forEach(ImageSimilarFinder::findSimilar);
 	}
 }
